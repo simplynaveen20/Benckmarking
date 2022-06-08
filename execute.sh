9ca2624b09fca99777187606b9fdfeab451eaec1
Ravi Tella <RaviTella@users.noreply.github.com>
2022-06-08 18:47:39 -0400

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -27,7 +27,7 @@ cp -r  ./azurecosmos/target/ /tmp/ycsb
 cp -r ./azurecosmos/conf/* /tmp/ycsb
 cd /tmp/ycsb/
 echo "########## Extracting YCSB binary ##########"
-tar xfvz ycsb-azurecosmos-binding-0.18
+tar xfvz ycsb-azurecosmos-binding-0.18.0-SNAPSHOT.tar.gz
 echo "########## Creating YCSB docker image ##########"
 docker build . -t ycsb-cosmos
 
