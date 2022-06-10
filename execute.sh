eb85c4f7770babfeb0b0400905e70b89883f25e4
Naveen Kumar Singh <simplynaveen20@gmail.com>
2022-06-10 16:02:15 -0400

    updating echo comment

--- execute.sh
+++ execute.sh
@@ -42,12 +42,12 @@ cd ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
 
 
 #Execute YCSB test
-echo "########## Executing YCSB tests@@@@@###########"
+echo "########## Executing YCSB tests ###########"
 uri=$COSMOS_URI primaryKey=$COSMOS_KEY workload_type=$WORKLOAD_TYPE ycsb_operation=$YCSB_OPERATION recordcount=$recordcount insertstart=$insertstart insertcount=$ITEM_COUNT_FOR_WRITE threads=$THREAD_COUNT target=$TARGET_OPERATIONS_PER_SECOND sh run.sh
 
 
 #Copy YCSB log to storage account 
-echo "########## Copying Results--@@@ to Storage ###########"
+echo "########## Copying Results to Storage ###########"
 cp /tmp/ycsb.log /home/benchmarking/"$VM_NAME-ycsb.log"
 sudo python3 converting_log_to_csv.py /home/benchmarking/"$VM_NAME-ycsb.log"
 sudo azcopy copy "$VM_NAME-ycsb.csv" "$RESULT_STORAGE_URL"
