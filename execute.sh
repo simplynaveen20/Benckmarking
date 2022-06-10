b64ec791a5759118b2bb0802d35fcb35c8b010de
Naveen Kumar Singh <simplynaveen20@gmail.com>
2022-06-10 14:53:32 -0400

    adding python script

--- execute.sh
+++ execute.sh
@@ -36,15 +36,35 @@ echo "########## Extracting YCSB ##########"
 tar xfvz ycsb-azurecosmos-binding-0.18.0-SNAPSHOT.tar.gz
 cp ./run.sh ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
 cp ./azurecosmos.properties ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
+cp ./aggregate_multiple_file_results.py ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
+cp ./converting_log_to_csv.py ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
 cd ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
 
 
 #Execute YCSB test
-echo "########## Executing YCSB tests###########"
+echo "########## Executing YCSB tests@@@@@###########"
 uri=$COSMOS_URI primaryKey=$COSMOS_KEY workload_type=$WORKLOAD_TYPE ycsb_operation=$YCSB_OPERATION recordcount=$recordcount insertstart=$insertstart insertcount=$ITEM_COUNT_FOR_WRITE threads=$THREAD_COUNT target=$TARGET_OPERATIONS_PER_SECOND sh run.sh
 
 
 #Copy YCSB log to storage account 
-echo "########## Copying Results to Storage ###########"
+echo "########## Copying Results--@@@ to Storage ###########"
 cp /tmp/ycsb.log /home/benchmarking/"$VM_NAME-ycsb.log"
+sudo python3 converting_log_to_csv.py /home/benchmarking/"$VM_NAME-ycsb.log"
+sudo azcopy copy "$VM_NAME-ycsb.csv" "$RESULT_STORAGE_URL"
 sudo azcopy copy "/home/benchmarking/$VM_NAME-ycsb.log" "$RESULT_STORAGE_URL"
+
+if [ $MACHINE_INDEX -eq 1 ]; then
+  echo "Sleeping on VM1 for 5 min"
+  sleep 5m
+  cd /home/benchmarking
+  mkdir "aggregation"
+  cd aggregation
+  index_for_regex=`expr index "$RESULT_STORAGE_URL" '?'`
+  regex_to_append="/*"
+  url_first_part=$(echo $RESULT_STORAGE_URL| cut -c 1-$((index_for_regex-1)))
+  url_second_part=$(echo $RESULT_STORAGE_URL| cut -c $((index_for_regex))-${#RESULT_STORAGE_URL})
+  new_storage_url="$url_first_part$regex_to_append$url_second_part"
+  sudo azcopy copy $new_storage_url '/home/benchmarking/aggregation' --recursive=true
+  sudo python3 /tmp/ycsb/ycsb-azurecosmos-binding-0.18.0-SNAPSHOT/aggregate_multiple_file_results.py /home/benchmarking/aggregation
+  sudo azcopy copy aggregation.csv "$RESULT_STORAGE_URL"
+fi
