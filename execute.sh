0e8741e3b6fb24bca1282f54bb8f654c87b0dcb2
Naveen Kumar Singh <simplynaveen20@gmail.com>
2022-06-14 13:07:27 -0400

    fixing sytax issue

--- execute.sh
+++ execute.sh
@@ -10,6 +10,7 @@ echo "##########VM Name###########: $VM_NAME"
 echo "##########ITEM_COUNT_FOR_WRITE###########: $ITEM_COUNT_FOR_WRITE"
 echo "##########MACHINE_INDEX###########: $MACHINE_INDEX"
 echo "##########YCSB_OPERATION_COUNT###########: $YCSB_OPERATION_COUNT"
+echo "##########YCSB_OPERATION###########: $YCSB_OPERATION"
 
 insertstart=$((ITEM_COUNT_FOR_WRITE* (MACHINE_INDEX-1)))
 recordcount=$((ITEM_COUNT_FOR_WRITE* MACHINE_INDEX))
@@ -42,16 +43,17 @@ cp ./converting_log_to_csv.py ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
 cd ./ycsb-azurecosmos-binding-0.18.0-SNAPSHOT
 
 
-#Execute YCSB test
-echo "########## Executing YCSB tests ###########"
-uri=$COSMOS_URI primaryKey=$COSMOS_KEY workload_type=$WORKLOAD_TYPE ycsb_operation=$YCSB_OPERATION recordcount=$recordcount insertstart=$insertstart insertcount=$ITEM_COUNT_FOR_WRITE threads=$THREAD_COUNT target=$TARGET_OPERATIONS_PER_SECOND sh run.sh
+##Load operation for YCSB tests
+echo "########## Load operation for YCSB tests ###########"
+uri=$COSMOS_URI primaryKey=$COSMOS_KEY workload_type=$WORKLOAD_TYPE ycsb_operation="load" recordcount=$recordcount insertstart=$insertstart insertcount=$ITEM_COUNT_FOR_WRITE threads=$THREAD_COUNT target=$TARGET_OPERATIONS_PER_SECOND sh run.sh
 
-if [ $YCSB_OPERATION == "run" ]; then
+#Execute YCSB test
+if [ "$YCSB_OPERATION" = "run" ]; then
   # Clearing log file from above load operation  
   sudo rm -f /tmp/ycsb.log
   echo "########## Inside RUN ###########"
   uri=$COSMOS_URI primaryKey=$COSMOS_KEY workload_type=$WORKLOAD_TYPE ycsb_operation=$YCSB_OPERATION recordcount=$recordcount operationcount=$YCSB_OPERATION_COUNT threads=$THREAD_COUNT target=$TARGET_OPERATIONS_PER_SECOND sh run.sh
-fi  
+fi
 
 #Copy YCSB log to storage account 
 echo "########## Copying Results to Storage ###########"
