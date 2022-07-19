3dfe88308d797de66e899438a66b69048d7ebb2b
Naveen Kumar Singh <simplynaveen20@gmail.com>
2022-07-19 16:27:49 -0400

    fixing execute.sh

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -2,7 +2,12 @@
 
 cloud-init status --wait
 echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
+echo "##########DEPLOYMENT_NAME###########: $DEPLOYMENT_NAME"
+whoami
+pwd
 # Running below commands in background, arm template completion wont wait on this
 # stdout and stderr logs will go in <$HOME>/<$DEPLOYMENT_NAME>.out and <$HOME>/<$DEPLOYMENT_NAME>.err
-curl -o custom-script.sh $CUSTOM_SCRIPT_URL
-nohup bash custom-script.sh > "${HOME}/${DEPLOYMENT_NAME}.out" 2> "${HOME}/${DEPLOYMENT_NAME}.err" &
+sudo curl -o custom-script.sh $CUSTOM_SCRIPT_URL
+echo "${HOME}/${DEPLOYMENT_NAME}.out"
+echo "${HOME}/${DEPLOYMENT_NAME}.err"
+sudo nohup bash custom-script.sh > "${HOME}/${DEPLOYMENT_NAME}.out" 2> "${HOME}/${DEPLOYMENT_NAME}.err" &
