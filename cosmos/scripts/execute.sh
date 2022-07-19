df0d04fcdf36b4e71582452c53ebb6bd61e9266a
Naveen Kumar Singh <simplynaveen20@gmail.com>
2022-07-19 16:51:04 -0400

    fixing execute.sh

--- cosmos/scripts/execute.sh
+++ cosmos/scripts/execute.sh
@@ -7,7 +7,7 @@ whoami
 pwd
 # Running below commands in background, arm template completion wont wait on this
 # stdout and stderr logs will go in <$HOME>/<$DEPLOYMENT_NAME>.out and <$HOME>/<$DEPLOYMENT_NAME>.err
-sudo curl -o custom-script.sh $CUSTOM_SCRIPT_URL
+curl -o custom-script.sh $CUSTOM_SCRIPT_URL
 echo "${HOME}/${DEPLOYMENT_NAME}.out"
 echo "${HOME}/${DEPLOYMENT_NAME}.err"
-sudo nohup bash custom-script.sh > "${HOME}/${DEPLOYMENT_NAME}.out" 2> "${HOME}/${DEPLOYMENT_NAME}.err" &
+nohup bash custom-script.sh > "/home/$ADMIN_USER_NAME/${DEPLOYMENT_NAME}.out" 2> "/home/$ADMIN_USER_NAME}/${DEPLOYMENT_NAME}.err" &
